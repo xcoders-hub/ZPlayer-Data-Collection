bc6604a65274afbc797186afb193e141234f36e2
zakariya1057 <zakaria2011@live.no>
2020-06-03 20:10:03 +0100

    Vidcloud Addtional Sources

--- Sources/WatchSeries/Servers/Vidcloud.php
+++ Sources/WatchSeries/Servers/Vidcloud.php
@@ -6,6 +6,7 @@ use Data\Data;
 use Shared\Request;
 use Exception;
 use Shared\Validator;
+use Symfony\Component\DomCrawler\Crawler;
 
 class Vidcloud extends Request {
 
@@ -15,22 +16,25 @@ class Vidcloud extends Request {
         $this->logger = $logger;
         $this->config = $config;
         $this->endpoint = 'https://vidcloud9.com/ajax.php?';
+        $this->download_endpoint = 'https://vidcloud9.com/download?';
     }
 
     public function video_locations($id){
+        global $vid_sources;
+
+        $vid_sources = [];
+
         $url = $this->endpoint.$id;
 
         $response = $this->request($url,'GET',array('x-requested-with' => 'XMLHttpRequest'));
         $content = $this->parse_json($response);
-
-        $sources = [];
 
         if($content && property_exists($content,'source')){
 
             foreach($content->source as $video){
                 $url = $video->file;
                 
-                preg_match('/m3u8|st3.cdnfile/i',$url,$matches);
+                preg_match('/\/\/m(:?2|3|5)x|st3.cdnfile/i',$url,$matches);
     
                 if(!$matches && $this->validate_url($url)){
     
@@ -39,7 +43,7 @@ class Vidcloud extends Request {
                     $source->url = $url;
                     $source->server_name = 'vidcloud';
         
-                    $sources[] = $source;
+                    $vid_sources[] = $source;
     
                 }
     
@@ -47,8 +51,36 @@ class Vidcloud extends Request {
             
         }
 
+        $url = $this->download_endpoint . $id;
 
-        return $sources;
+        $response = $this->request($url,'GET',array('x-requested-with' => 'XMLHttpRequest'));
+        $content = $this->parse_html($response);
+        
+        $content->filter('.mirror_link .dowload a')->each(function(Crawler $node, $i){
+            global $vid_sources;
+
+            $quality = preg_replace('/Download| \(|\)| - mp4/i','',$node->text());
+            $link = $node->attr('href');
+
+            preg_match('/ads|Xstreamcdn/i',$quality,$matches);
+
+            if(!$matches){
+
+                $this->logger->debug('Found: '.$quality);
+
+                $source = new Data();
+                $source->quality = trim($quality);
+                $source->url = str_replace(' ','%20',$link);
+                $source->server_name = 'VidCloud';
+    
+                $vid_sources[] = $source;
+
+            }
+
+        });
+
+        return $vid_sources;
+
     }
 }
 
