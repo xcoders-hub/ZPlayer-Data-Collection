4595112ea89953a9dc3b5bd2b2565b8a4ea9e87b
zakariya1057 <zakaria2011@live.no>
2020-06-13 20:39:01 +0100

    Fixed Logger Permission Bug

--- Shared/Logger.php
+++ Shared/Logger.php
@@ -13,17 +13,21 @@ $date = date('d-m-Y');
 $logs_directory = __DIR__."/../Logs/$date";
 
 if(!file_exists($logs_directory)){
-    mkdir($logs_directory);
+    $oldmask = umask(0);
+    mkdir($logs_directory, 0777);
+    umask($oldmask);
+} else {
+
 }
 
 $logger->pushHandler(new StreamHandler('php://stdout', Logger::DEBUG));
 
-$logger->pushHandler(new StreamHandler($logs_directory.'/debug.log', Logger::DEBUG));
-$logger->pushHandler(new StreamHandler($logs_directory.'/info.log', Logger::INFO));
-$logger->pushHandler(new StreamHandler($logs_directory.'/error.log', Logger::ERROR));
-$logger->pushHandler(new StreamHandler($logs_directory.'/critical.log', Logger::CRITICAL));
-$logger->pushHandler(new StreamHandler($logs_directory.'/alert.log', Logger::ALERT));
-$logger->pushHandler(new StreamHandler($logs_directory.'/warning.log', Logger::WARNING));
-$logger->pushHandler(new StreamHandler($logs_directory.'/notice.log', Logger::NOTICE));
+$logger->pushHandler(new StreamHandler($logs_directory.'/debug.log', Logger::DEBUG,true,0777));
+$logger->pushHandler(new StreamHandler($logs_directory.'/info.log', Logger::INFO,true,0777));
+$logger->pushHandler(new StreamHandler($logs_directory.'/error.log', Logger::ERROR,true,0777));
+$logger->pushHandler(new StreamHandler($logs_directory.'/critical.log', Logger::CRITICAL,true,0777));
+$logger->pushHandler(new StreamHandler($logs_directory.'/alert.log', Logger::ALERT,true,0777));
+$logger->pushHandler(new StreamHandler($logs_directory.'/warning.log', Logger::WARNING,true,0777));
+$logger->pushHandler(new StreamHandler($logs_directory.'/notice.log', Logger::NOTICE,true,0777));
 
 ?>
