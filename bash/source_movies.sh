02fec8d1fc35b9d5a0a5f43e82e0b358013005d1
zakariya1057 <zakaria2011@live.no>
2020-06-19 11:23:18 +0100

    Removed unused bash scripts

--- bash/source_movies.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-php "$DIR/../Sources/Main.php" watchseries movies > "$DIR/../output/sources_movies.log" &
--- bash/sources_update.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-php "$DIR/../Sources/Main.php" Watchseries update > "$DIR/../output/sources_update.log" &
