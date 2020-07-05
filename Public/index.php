6f4cea448410454e36675d7ddad92072747431a4
zakariya1057 <zakaria2011@live.no>
2020-07-05 19:26:05 +0100

    Improved Recent Search

--- Public/index.php
+++ Public/index.php
@@ -5,11 +5,14 @@ require_once __DIR__.'/../Shared/Logger.php';
 
 use Details\Information;
 use Details\Recent;
+use phpDocumentor\Reflection\DocBlock\Tags\Example;
 use Sources\WatchSeries\Server;
 
 use Shared\Config;
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $reponse_json = '{}';
 
     try {
 
@@ -20,17 +23,31 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $logger->debug('------- Incoming Request --------');
     
         $conf = new Config($logger);
-        $Config = $conf->load_config($request->data->user_key);
+        $data = $request->data;
+
+        $Config = $conf->load_config($data->user_key);
     
-        $search_type = strtolower($request->data->type);
-        $content_type = strtolower($request->data->content_type);
+        $search_type = strtolower($data->type);
+        $content_type = strtolower($data->content_type);
     
         $logger->debug("------- Search Type: $search_type --------");
     
         if($search_type == 'search_sources'){
             $server = new Server($Config,$logger);
             $reponse_json = $server->sources($request);
-        } elseif($search_type == 'search_released'){
+        } elseif($search_type == 'search_recent'){
+            
+            if(property_exists($data,'return_location')){
+                $return_location = $data->return_location;
+
+                if (filter_var($return_location, FILTER_VALIDATE_URL) === FALSE) {
+                    throw new Exception('Invalid Return Location Provided');
+                }
+
+                $Config->api->url = $return_location;
+                $logger->notice('Data Return Location: '.$return_location);
+            }
+
             $released = new Recent($Config,$logger);
             $released->recent_content($content_type);
         } elseif($search_type == 'search_similar'){
